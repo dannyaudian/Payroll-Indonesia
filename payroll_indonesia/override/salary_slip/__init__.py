d81fc4d6b20e7325fea4333c3f4b1ffe97b4f126
dannyaudian <danny.a.pratama@gmail.com>
2025-06-28 18:00:29 +0700

    Update __init__.py

--- payroll_indonesia/override/salary_slip/__init__.py
+++ payroll_indonesia/override/salary_slip/__init__.py
@@ -3,13 +3,11 @@
 from __future__ import unicode_literals
 
 # Import functions that need to be accessible from outside
-from payroll_indonesia.override.salary_slip import calculate_ytd_and_ytm
 from payroll_indonesia.override.salary_slip.bpjs_calculator import calculate_bpjs_components
 from payroll_indonesia.override.salary_slip.tax_calculator import calculate_tax_components
 
 # Define what should be accessible when importing from this module
 __all__ = [
-    'calculate_ytd_and_ytm',
     'calculate_bpjs_components',
     'calculate_tax_components'
 ]
