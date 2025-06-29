22b9fab8b9609ece9a60d4d4f21356c819ca091f
dannyaudian <danny.a.pratama@gmail.com>
2025-06-29 10:09:21 +0700

    Update __init__.py

--- payroll_indonesia/config/__init__.py
+++ payroll_indonesia/config/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.0.1"
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, PT. Innovasi Terbaik Bangsa and contributors
+# For license information, please see license.txt
+# Last modified: 2025-06-29 03:10:23 by dannyaudian
+
+"""
+Configuration module for Payroll Indonesia.
+Imports from main config.py to maintain compatibility.
+"""
+
+from payroll_indonesia.config import get_config, get_live_config
+
+__all__ = ["get_config", "get_live_config"]
