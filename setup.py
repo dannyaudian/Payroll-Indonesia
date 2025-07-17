fec0185d3c797c48fa2a8b32a0163c2119ace401
dannyaudian <danny.a.pratama@gmail.com>
2025-07-17 14:30:15 +0700

    Add license and Python version metadata

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     description="Payroll module for Indonesian companies",
     author="PT. Innovasi Terbaik Bangsa",
     author_email="danny.a.pratama@cao-group.co.id",
+    license="MIT",
+    python_requires=">=3.10",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
