eee16d7439abd9a854a396f4766ca69e12b4e012
dannyaudian <danny.a.pratama@gmail.com>
2025-08-02 11:16:19 +0700

    refactor: align annual payroll totals with docfields

--- payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history/annual_payroll_history.py
+++ payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history/annual_payroll_history.py
@@ -3,18 +3,14 @@ from frappe.model.document import Document
 class AnnualPayrollHistory(Document):
     def validate(self):
         # Inisialisasi total
-        self.total_bruto = 0
-        self.total_pengurang_netto = 0
-        self.total_biaya_jabatan = 0
-        self.total_netto = 0
-        self.total_pkp = 0
-        self.total_pph21 = 0
+        self.bruto_total = 0
+        self.netto_total = 0
+        self.pkp_annual = 0
+        self.pph21_annual = 0
 
         # Penjumlahan dari child table
         for row in self.monthly_details:
-            self.total_bruto += row.bruto or 0
-            self.total_pengurang_netto += row.pengurang_netto or 0
-            self.total_biaya_jabatan += row.biaya_jabatan or 0
-            self.total_netto += row.netto or 0
-            self.total_pkp += row.pkp or 0
-            self.total_pph21 += row.pph21 or 0
+            self.bruto_total += row.bruto or 0
+            self.netto_total += row.netto or 0
+            self.pkp_annual += row.pkp or 0
+            self.pph21_annual += row.pph21 or 0
