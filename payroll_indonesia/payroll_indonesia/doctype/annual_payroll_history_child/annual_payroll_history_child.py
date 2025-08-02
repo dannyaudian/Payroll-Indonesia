494ea99ba8c87e9757f673c45d6c651d6486a846
dannyaudian <danny.a.pratama@gmail.com>
2025-08-02 14:20:07 +0700

    fix

--- payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history_child/annual_payroll_history_child.py
+++ payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history_child/annual_payroll_history_child.py
@@ -3,5 +3,4 @@ from frappe.model.document import Document
 
 
 class AnnualPayrollHistoryChild(Document):
-    def get_title(self):
-        return f"Bulan {self.bulan} - Netto {frappe.utils.fmt_money(self.netto)}"
+    pass
