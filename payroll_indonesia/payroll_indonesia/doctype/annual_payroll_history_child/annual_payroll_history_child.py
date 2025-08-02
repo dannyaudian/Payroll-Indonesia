fb1e7890c3cb46a28ecb99f5a9db41ce2dbcd832
dannyaudian <danny.a.pratama@gmail.com>
2025-08-02 11:17:08 +0700

    Add title formatting for annual payroll history child

--- payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history_child/annual_payroll_history_child.py
+++ payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history_child/annual_payroll_history_child.py
@@ -1,4 +1,7 @@
+import frappe
 from frappe.model.document import Document
 
+
 class AnnualPayrollHistoryChild(Document):
-    pass
+    def get_title(self):
+        return f"Bulan {self.bulan} - Netto {frappe.utils.fmt_money(self.netto)}"
