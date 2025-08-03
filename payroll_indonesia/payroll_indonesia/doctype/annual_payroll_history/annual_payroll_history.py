7e1e42e32132fac9d1055ab5e28193a0bc2f9dfc
dannyaudian <danny.a.pratama@gmail.com>
2025-08-03 19:00:33 +0700

    feat: validate annual payroll totals

--- payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history/annual_payroll_history.py
+++ payroll_indonesia/payroll_indonesia/doctype/annual_payroll_history/annual_payroll_history.py
@@ -13,13 +13,34 @@ class AnnualPayrollHistory(Document):
             "pkp_annual": 0,
             "pph21_annual": 0,
         }
+        pengurang_netto_total = 0
+        biaya_jabatan_total = 0
 
         self.koreksi_pph21 = self.koreksi_pph21 or 0
         self.ptkp_annual = self.ptkp_annual or 0
 
         for row in self.monthly_details or []:
-            totals["bruto_total"] += flt(row.bruto)
-            totals["netto_total"] += flt(row.netto)
+            bruto = flt(row.bruto)
+            pengurang_netto = flt(getattr(row, "pengurang_netto", 0))
+            biaya_jabatan = flt(getattr(row, "biaya_jabatan", 0))
+            netto = flt(row.netto)
+
+            pengurang_netto_total += pengurang_netto
+            biaya_jabatan_total += biaya_jabatan
+
+            calculated_netto = bruto - pengurang_netto - biaya_jabatan
+            if abs(calculated_netto - netto) > 0.1:
+                frappe.logger("payroll_indonesia").warning(
+                    {
+                        "message": "netto mismatch",
+                        "calculated_netto": calculated_netto,
+                        "stored_netto": netto,
+                        "row": row.as_dict(),
+                    }
+                )
+
+            totals["bruto_total"] += bruto
+            totals["netto_total"] += netto
             totals["pkp_annual"] += flt(row.pkp)
             totals["pph21_annual"] += flt(row.pph21)
 
@@ -28,6 +49,8 @@ class AnnualPayrollHistory(Document):
         frappe.logger("payroll_indonesia").debug(
             {
                 **totals,
+                "pengurang_netto_total": pengurang_netto_total,
+                "biaya_jabatan_total": biaya_jabatan_total,
                 "koreksi_pph21": self.koreksi_pph21,
                 "ptkp_annual": self.ptkp_annual,
             }
